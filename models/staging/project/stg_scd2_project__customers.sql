e00236219c723b3fbbe7ec0572eeec31c37450b9
Filippo Ridolfi <filippo.ridolfi@sdggroup.com>
2025-10-30 15:13:51 +0100

    Update docs

--- models/staging/project/stg_scd2_project__customers.sql
+++ models/staging/project/stg_scd2_project__customers.sql
@@ -6,40 +6,23 @@
     ) 
 }}
 
-/*
-    Carica i dati di origine dalla tabella appropriata in base al tipo di esecuzione
-*/
-
 with source_data as (
     select
         *,
         {{ dbt_utils.generate_surrogate_key(['customer_cd', 'last_update']) }} as customer_update_id
     from 
     {% if is_incremental() %}
-    {{ source('project', 'customers_t1') }}
+        {{ source('project', 'customers_t1') }}
+
+        where last_update > (select max(last_update) from {{ this }})
     {% else %}
-    {{ source('project', 'customers_t0') }}
+        {{ source('project', 'customers_t0') }}
     {% endif %}
 ),
 
-{% if is_incremental() %}
-
-/*
-    Isola solo i record della sorgente che sono nuovi o modificati
-*/
-changed_records_source as (
-    select
-        s.*
-    from source_data s
-    -- Solo i record la cui 'customer_update_id' NON esiste già nel modello ({{ this }})
-    left join {{ this }} as t
-        on s.customer_update_id = t.customer_update_id
-    where t.customer_update_id is null
-),
-{% endif %}
-
 /*
     Prepara le nuove versioni dei record
+    'source_data' contiene GIÀ solo i record nuovi o modificati
 */
 rows_to_insert as (
     select
@@ -56,19 +39,15 @@ rows_to_insert as (
         null as valid_to,          
         true as is_current         
     from 
-    {% if is_incremental() %}
-        changed_records_source     
-    {% else %}
-        source_data                
-    {% endif %}
+        source_data
 ),
 
 {% if is_incremental() %}
 
 /*
     Prepara i record esistenti da aggiornare/chiudere
+    Questi sono i record GIÀ presenti in {{ this }} che devono essere chiusi
 */
-
 rows_to_update as (
     select
         t.customer_update_id,
@@ -81,20 +60,23 @@ rows_to_update as (
         t.is_deleted,
         t.dbt_updated_at,
         t.valid_from,
-        s.last_update as valid_to, 
+        s.last_update as valid_to, -- Prende la data di aggiornamento dal nuovo record
         false as is_current        
     from {{ this }} as t
     
-    inner join changed_records_source s
+    -- Unisce i record sorgente (nuovi) con la tabella di destinazione (attuale)
+    -- sulla chiave di business (customer_cd)
+    inner join source_data s
         on t.customer_cd = s.customer_cd
+    -- Filtra solo i record attualmente attivi in destinazione
     where t.is_current = true
 ),
+
 {% endif %}
 
 /*
     Combina i record da inserire e i record esistenti da aggiornare/chiudere
 */
-
 final as (
     select * from rows_to_insert
     
